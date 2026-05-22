8efa9edf6caed5e8b69a8a2af9adfb7f51031bff
AhmadTaufik24 <160514846+AhmadTaufik24@users.noreply.github.com>
2026-05-22 09:02:50 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,7 @@ function updateTime() {
     let greetingText = '';
     
     if (hour >= 5 && hour < 12) greetingText = 'Good Morning, Taufik.';
-    else if (hour >= 12 && hour < 15) greetingText = 'Good Afternoo, Taufik.';
+    else if (hour >= 12 && hour < 15) greetingText = 'Good Afternoon, Taufik.';
     else if (hour >= 15 && hour < 19) greetingText = 'Good Evening, Taufik.';
     else greetingText = 'Good Night, Taufik.';
 
